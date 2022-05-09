b16fee742ca2eaa74f943fb0196ba9983179fcd9
ilyawebdevelop <ilia966@tut.by>
2022-05-09 21:40:03 +0300

    15 pravok

---
+++ src/src/js/components.js
@@ -0,0 +1,8 @@
+import './components/modal.js';
+import './components/lightgallery.umd.js';
+import './components/lightgallery.min.js';
+
+// lightgallery plugins
+import './components/lg-thumbnail.umd.js';
+import './components/lg-zoom.umd.js';
+
